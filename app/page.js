7ec50fc4650f0ab6ce2d3c329a342b89ae3040d5
Sukrit <sukrit3007@outlook.com>
2023-10-13 19:05:47 +0530

    error 3

--- app/page.js
+++ app/page.js
@@ -20,6 +20,7 @@ export default function Home() {
         <div className="min-h-screen min-w-full relative">
           <Image
             src={bgwhite}
+            alt=''
             className="min-w-full bg-cover bg-center absolute left-0 right-0 bottom-0 -z-20 sm:h-"
           />
           <Hero className="relative z-10" />
--- app/pages/TheAuto.jsx
+++ app/pages/TheAuto.jsx
@@ -5,17 +5,16 @@ import Image from 'next/image';
 import autoimg from '../../public/the-auto-img.svg';
 import dynamic from "next/dynamic";
 
-
 const TheAuto = () => {
   return (
     <div className="min-w-full bg-white">
       <div className="container mx-auto flex flex-col items-center py-16 lg:flex-row lg:justify-center gap-y-8">
         <div className="lg:w-1/2 lg:pr-10 order-2 lg:order-1">
           <h1 className="text-4xl lg:text-5xl font-bold mb-4 text-left text-black">
-            The auto-suggest layer<br />for human thinking
+            The auto-suggest layer<br />&apos;for human thinking&apos;
           </h1>
           <p className="text-lg mb-6 text-left text-black">
-            Connect fragments of information from siloed sources faster than ever before. Your organization's collective memory at your fingertips.
+            Connect fragments of information from siloed sources faster than ever before. Your organization&apos;s collective memory at your fingertips.
           </p>
           <Button className="rounded-3xl flex flex-row gap-x-2 bg-blue-700 text-white">
             Learn More <ArrowRightIcon className="mr-2 h-4 w-4" />
@@ -34,5 +33,4 @@ const TheAuto = () => {
   );
 };
 
-
-export default dynamic (() => Promise.resolve(TheAuto), {ssr: false})
+export default dynamic(() => Promise.resolve(TheAuto), { ssr: false });
